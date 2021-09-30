b6c91953b496d0c1095057b42a9f822d04aae37f
Adrian Danao-Schroeder <47489123+andanao@users.noreply.github.com>
2021-09-30 11:40:39 -0400

    added fn to airtable

--- airtable.py
+++ airtable.py
@@ -1,3 +1,4 @@
+#%%
 import requests
 import json
 import pandas as pd
@@ -14,8 +15,24 @@ class Airtable():
                 self.base_id = f.read()
         else:
             self.base_id = base_id
-        
+    
+    def get_table(self,params= (),table_name = False):
+        if not table_name:
+            with open('keys/table_name.txt') as f:
+                self.table_name = f.read()
+        else:
+            self.table_name = table_name
+
+    def get_dataframe(self):
+        pass
+
+
+
+
+
 
 if __name__=="__main__":
     air = Airtable()
-    
+
+
+# %%
