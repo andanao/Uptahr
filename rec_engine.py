b49f8a1abe66cd41da6cf35e22b06eb1083d8eae
Adrian Danao-Schroeder <47489123+andanao@users.noreply.github.com>
2021-09-30 16:34:27 -0400

    started building rec engine

--- rec_engine.py
+++ rec_engine.py
@@ -1,3 +1,20 @@
+#%%
 from airtable import Airtable
 from user import User
 
+# %%
+class Engine():
+    def __init__(self) -> None:
+        self.get_users
+    
+    def get_users(self,num=10):
+        self.users = []
+        for i in range(num):
+            self.users.append(User())
+
+
+
+if __name__ == "__main__":
+    eng = Engine()
+    user0 = eng.users[0]
+# %%
