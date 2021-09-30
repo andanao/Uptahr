ab05370caf6992cf6da7f2994cdb960eff220d60
Adrian Danao-Schroeder <47489123+andanao@users.noreply.github.com>
2021-09-30 16:35:34 -0400

    changed user.tags to an OrderedDict

--- user.py
+++ user.py
@@ -1,12 +1,13 @@
 #%%
 import numpy as np
+from collections import OrderedDict
 
 class User:
     def __init__(self) -> None:
         self.id = np.random.randint(10000)
         self.articles_read = []
         # self.likes = np.zeros(10) # 
-        self.tags_read = {}
+        self.tags_read = OrderedDict()
 
 
 if __name__=="__main__":
