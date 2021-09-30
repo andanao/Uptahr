18aa20f4355181e5eeb7df0d6241caed029c0030
Adrian Danao-Schroeder <47489123+andanao@users.noreply.github.com>
2021-09-30 11:33:53 -0400

    test for airtable.py

--- airtable.py
+++ airtable.py
@@ -4,8 +4,18 @@ import pandas as pd
 
 class Airtable():
     def __init__(self,api_key = False, base_id = False) -> None:
-        with open('keys/api_key.txt') as f:
-            self.api_key = f.read()
-        with open('keys/base_id.txt') as f:
-            self.base_id = f.read()
+        if not api_key:
+            with open('keys/api_key.txt') as f:
+                self.api_key = f.read()
+        else:
+            self.api_key = api_key
+        if not base_id:
+            with open('keys/base_id.txt') as f:
+                self.base_id = f.read()
+        else:
+            self.base_id = base_id
         
+
+if __name__=="__main__":
+    air = Airtable()
+    
