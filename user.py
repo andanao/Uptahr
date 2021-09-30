ced06ba4b2475a9fc78b3e6f386c8e925b63f9b7
Adrian Danao-Schroeder <47489123+andanao@users.noreply.github.com>
2021-09-30 11:09:08 -0400

    user py created and tested

---
+++ user.py
@@ -0,0 +1,16 @@
+#%%
+import numpy as np
+
+class User:
+    def __init__(self) -> None:
+        self.id = np.random.randint(10000)
+        self.articles_read = []
+        # self.likes = np.zeros(10) # 
+        self.tags_read = {}
+
+
+if __name__=="__main__":
+    user = User()
+    print("User ID\t\t"+str(user.id))
+    print("articles_read\t"+str(user.articles_read))
+    print("tags_read\t"+str(user.tags_read))
